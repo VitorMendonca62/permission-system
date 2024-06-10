446f040fb5fb9748cd09def5c83f7617f289110c
VitorMendonca62 <vitorqueiroz325@gmail.com>
2024-06-10 14:27:29 -0300

    Criando setup do projeto

---
+++ server/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import routes from './routes';
+import prisma from './prisma/client';
+import cors from 'cors';
+
+const App = express();
+
+prisma.$connect().then(() => console.log('Database is running'));
+
+// Accept JSONs
+App.use(express.urlencoded({ extended: true }));
+App.use(express.json());
+
+// CORS
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+  allowedHeaders: 'Content-Type,Authorization',
+};
+App.use(cors(corsOptions));
+
+App.use(routes);
+
+export { App, prisma };
