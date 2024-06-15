7c7cacb5878962634f9116551d6146646fbb30f3
VitorMendonca62 <vitorqueiroz325@gmail.com>
2024-06-15 16:39:23 -0300

    ajustes finais

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import { App } from './app';
 
-const PORT = process.env.PORT || 4257;
+const PORT = process.env.PORT || 5275;
 
 App.listen(PORT, () => console.log(`Server listing in port ${PORT}`));
